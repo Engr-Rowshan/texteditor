022804b52d2347366616012ae1657dd85e22867f
Rowshan <engr.rowshan@gmail.com>
2019-04-22 00:36:21 +0600

    Started coding
    Init

--- js/textEditor.js
+++ js/textEditor.js
@@ -1,23 +1,56 @@
 (function( $ ) {
  
     $.fn.textEditor = function(options) {
- 
-        var settings = $.extend({
+        let originalObjects = new Array();
+        let createdObjects = new Array();
+
+        let settings = $.extend({
             // These are the defaults.
-            color: "#556b2f",
+            css: true,
+            height: '200px',
+            width: '100%',
             backgroundColor: "white"
         }, options );
 
-        // Greenify the collection based on the settings variable.
-        return this.css({
-            color: settings.color,
-            backgroundColor: settings.backgroundColor
+        //Load Default Css file
+        if(settings.css){
+            $("<link/>", {
+                rel: "stylesheet",
+                type: "text/css",
+                href: "css/textEditor.css"
+             }).appendTo("head");
+        }
+
+
+        //This will confirm plugin work with multi object
+        return this.each(function(){
+            create(this);
         });
-        
+
+
+        //Create the elements
+        function create(e){
+            originalObjects.push(e);
+            let container = $('<div></div>').addClass('textEditor').css('width',settings.width).css('height',settings.height);
+            let toolBar = $("<div></div>").addClass('toolBox');
+            let frameBox = $('<div></div>').addClass('frameBox');
+            let frame = $('<iframe></iframe>').addClass('frame');
+
+            $(e).css('display','none'); //Hide the main object
+            frameBox.append(frame);
+            container.append(toolBar).append(frameBox);
+
+            container.insertBefore(e);
+        }
+
+        // // Greenify the collection based on the settings variable.
+        // return this.css({
+        //     color: settings.color,
+        //     backgroundColor: settings.backgroundColor
+        // });
+        alert('Plugins Called');
  
     };
  
 }( jQuery ));
  
-// Usage example:
-$( "a" ).showLinkLocation();
