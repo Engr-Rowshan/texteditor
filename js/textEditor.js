72a917fa314e630d3aba8a56a5d6cf2b82635282
Rowshan <engr.rowshan@gmail.com>
2019-04-27 01:05:21 +0600

    Update textEditor.js

--- js/textEditor.js
+++ js/textEditor.js
@@ -11,8 +11,8 @@
             css: true,                  //Set if default css file will be loaded with plugins
             height: '200px',            //Default Height fo container
             width: '100%',              //Default width of the container
-            backgroundColor: "white",   //Default Backgroud Color of Container
-            buttons: ['bold'],
+            backgroundColor: "white",   //Default Background Color of Container
+            buttons: ['bold','italic'],
             loadFontAwesomeCss: true       //load Font Awesome
         }, options );
 
@@ -40,52 +40,84 @@
             create.apply(this);
         });
 
+        function enableEditMode () {
+            frame.document.designMode = 'On';
+        }
+
+        
         //Create the elements
         function create(){
             originalObjects.push(this);
             let container = $('<div></div>').addClass('textEditor').css('width',settings.width).css('height',settings.height);
             let toolBar = $("<div></div>").addClass('toolBox');
 
-            let btnObj1 = createButton('Bold','bold');
-            let btnObj2 = createButton('Italic','italic');
 
-            toolBar.append(btnObj1).append(btnObj2);
+            settings.buttons.forEach(function(i,k){
+                let letBtn = getDefaultButton(i);
+                toolBar.append(letBtn);
+            })
+
 
             let frameBox = $('<div></div>').addClass('frameBox');
-            let frame = $('<iframe></iframe>').addClass('frame');
+            let frame = $('<iframe></iframe>').addClass('frame').attr('id','frame').attr('name','frame');
 
             $(this).css('display','none'); //Hide the main object
             frameBox.append(frame);
             container.append(toolBar).append(frameBox);
 
             container.insertBefore(this);
+
+            enableEditMode();
+        }
+        
+        //This function will hold all the predefined buttons
+        function getDefaultButton(title){
+            let btn;
+            switch (title){
+                case 'bold':
+                    btn = {title:'bold',icon:'bold',className:'btn btn-primary',callBack:boldFunction};
+                break;
+                case 'italic': 
+                    btn = {title:'italic',icon:'italic',className:'btn btn-primary',callBack:italicFunction}
+                    break;
+                default:
+                    btn =  null;
+            }
+            if(btn != null) return createButtonX(btn);
+            return null;
         }
 
-        //A function to create button
-        function createButton(btnTitle,btnIcon = null ,btnClass = 'btn btn-primary' ,btnCallback = null){
-            let btnObj = $('<button></button>').addClass(btnClass);
+        function createButtonX(obj){
+            let btnObj = $('<button></button>').addClass(obj.className);
             
             //Set Icon
-            if(btnIcon != null){
-                let icon = $('<i></i>').addClass('fa fa-' + btnIcon);
+            if(obj.icon != null){
+                let icon = $('<i></i>').addClass('fa fa-' + obj.icon);
                 btnObj.append(icon);
             }else{
-                btnObj.text(btnTitle);
+                btnObj.text(obj.title);
             }
 
             //Set Callback function
-            if(btnCallback != null && btnCallback instanceof Function){
-                btnCallback.apply(this);        
-            }else{
-                //Check if predefined function exist
-                switch(btnTitle){
-                    case "bold":
-                        //Do Bold Work
-                        break;
-                }
+            if(obj.callBack != null && obj.callBack instanceof Function){
+                btnObj.on('click',obj.callBack);
             }
             return btnObj;
         }
     };
+
+
+    function boldFunction(){
+        execCmd('bold');
+    }
+
+    function italicFunction(){
+        execCmd('italic');
+    }
+
+    function execCmd (command) {
+        frame.document.execCommand(command, false, null);
+    }
+
 }( jQuery ));
  
